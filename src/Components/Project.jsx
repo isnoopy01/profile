9d8b71bcbcdf48cde5413983e504b22eada10c64
isnoopy01 <minhtoanbmt2001@gmail.com>
2021-10-30 12:38:34 +0700

    update

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -6,6 +6,7 @@ import htmlimg from '../Components/public/image/html5logo.jpg'
 import cssimg from '../Components/public/image/csslogo.png'
 import javaimg from '../Components/public/image/java.png'
 import databasebimg from '../Components/public/image/mysql.png'
+import phpimg from '../Components/public/image/php.png'
 
 
 
@@ -16,12 +17,13 @@ class Project extends Component {
         return (
        
             <section id="skillheader" className="flex-project-container">
-
+                
+                    <div><img src={javaimg} width="100" height="100" alt="wordpress"/></div>
                     <div><img src={pyimg} width="100" height="100" alt="python"/></div>
                     <div><img src={jsimg} width="100" height="100" alt="js"/></div>
                     <div><img src={htmlimg} width="100" height="100" alt="html"/></div>  
                     <div><img src={cssimg} width="100" height="100" alt="css"/></div>
-                    <div><img src={javaimg} width="100" height="100" alt="wordpress"/></div>
+                    <div><img src={phpimg} width="100" height="100" alt="db"/></div>
                     <div><img src={databasebimg} width="100" height="100" alt="db"/></div>
                 
             </section>
